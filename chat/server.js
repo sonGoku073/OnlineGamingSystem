fa2683e36a764077e2e3f4eec054589417882cfe
sonGoku073 <songokusupersaiyan073@gmail.com>
2024-09-24 10:29:28 +0530

    Update server.js

--- chat/server.js
+++ chat/server.js
@@ -1,18 +1,34 @@
-const io = require('socket.io')('https://onlinegamingplatform.vercel.app/'
-)
+const http = require('http');
+const socketIo = require('socket.io');
 
-const users = {}
+const server = http.createServer();
+const io = socketIo(server, {
+  cors: {
+    origin: 'https://onlinegamingplatform.vercel.app',
+    methods: ['GET', 'POST'],
+    credentials: true
+  }
+});
+
+const users = {};
 
 io.on('connection', socket => {
   socket.on('new-user', name => {
-    users[socket.id] = name
-    socket.broadcast.emit('user-connected', name)
-  })
+    users[socket.id] = name;
+    socket.broadcast.emit('user-connected', name);
+  });
+
   socket.on('send-chat-message', message => {
-    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
-  })
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] });
+  });
+
   socket.on('disconnect', () => {
-    socket.broadcast.emit('user-disconnected', users[socket.id])
-    delete users[socket.id]
-  })
-})
+    socket.broadcast.emit('user-disconnected', users[socket.id]);
+    delete users[socket.id];
+  });
+});
+
+// Start the server on port 3000
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
